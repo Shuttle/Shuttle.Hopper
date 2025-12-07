4fbb2f80ad5239472d01b0ca971899ca14f5e345
Eben <me@ebenroux.co.za>
2025-12-07 07:32:31 +0200

    Removed inheriting from EventArgs.

--- Shuttle.Hopper/Events/TransportEventArgs.cs
+++ Shuttle.Hopper/Events/TransportEventArgs.cs
@@ -2,7 +2,7 @@ using Shuttle.Core.Contract;
 
 namespace Shuttle.Hopper;
 
-public class TransportEventArgs(ITransport transport) : EventArgs
+public class TransportEventArgs(ITransport transport)
 {
     public ITransport Transport { get; } = Guard.AgainstNull(transport);
 }
